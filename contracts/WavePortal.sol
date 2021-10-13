a8d67f59163fcdb17e8a7c602d33c6e353fb5f32
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2021-10-13 08:52:53 -0500

    converted vars to private, added getter functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,24 +5,58 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    uint totalWaves;
-    uint lastWaveAt;
-    address lastWaver;
+    uint private _totalWaves;
+    uint private _lastWaveAt;
+    address private _lastWaver;
+    uint private _maxWaves;
+    address private _maxWaver;
+    mapping(address => uint) private _lastWaveTimestamp;
+    mapping(address => uint) private _waves;
 
     constructor() {
         console.log("I'm a smart contract. Look at me being super smart!");
     }
 
     function wave() public {
-        totalWaves += 1;
-        lastWaveAt = block.number;
-        lastWaver = msg.sender;
+        _totalWaves += 1;
+        _lastWaveAt = block.timestamp;
+        _lastWaver = msg.sender;
 
-        console.log("Wave sent! The last person to wave was %s at block number %d.", lastWaver, lastWaveAt);
+        console.log("Wave sent! The last person to wave was %s at block number %d.", _lastWaver, _lastWaveAt);
     }
 
     function getTotalWaves() public view returns (uint) {
-        console.log("You've got %d total waves. Noice!!!", totalWaves);
-        return totalWaves;
+        console.log("You've got %d total waves. Noice!!!", _totalWaves);
+        return _totalWaves;
+    }
+
+    function getLastWaveAt() public view returns (uint) {
+        console.log("The last wave was ", _lastWaveAt);
+        return _lastWaveAt;
+    }
+
+    function getLastWaver() public view returns (address) {
+        console.log("The last waver was ", _lastWaver);
+        return _lastWaver;
+    }
+
+    function getMaxWaves() public view returns (uint) {
+        console.log("The most waves is currently", _maxWaves);
+        return _maxWaves;
+    }
+
+    function getMaxWaver() public view returns (address) {
+        console.log("The highest waver is currently ", _maxWaver);
+        return _maxWaver;
+    }
+
+    function getWaverLast() public view returns (uint) {
+        console.log("You last waved at ", _lastWaveTimestamp[msg.sender]);
+        return _lastWaveTimestamp[msg.sender];
+    }
+
+    function getWaverWaves() public view returns (uint) {
+        console.log("Your total waves are ", _waves[msg.sender]);
+        return _waves[msg.sender];
     }
 }
