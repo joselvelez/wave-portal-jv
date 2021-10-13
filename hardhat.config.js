3d772d92ace33e2a0e7d8c8a9e9461b586179a2b
joselvelez <joselvelez@protonmail.com>
2021-10-12 20:20:04 -0500

    adding mm wallet support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -18,4 +20,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.API_URL,
+      accounts: [process.env.KEY],
+    },
+  },
 };
